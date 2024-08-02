b5b3e339bcf559cd4c1028a1dc794d1115e882f8
Dennis Kinanga <denniskinanga6@gmail.com>
2024-08-03 01:51:16 +0300

    dev

--- devices/schemas.py
+++ devices/schemas.py
@@ -17,4 +17,11 @@ class DeviceCreateSchema(Schema):
     name: str 
     location_id: int | None = None 
 
+class Error(Schema):
+    message: str 
+
+
+class DeviceLocationPatch(Schema):
+    location_id: int | None = None 
+
 
--- devices_backend/settings.py
+++ devices_backend/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'devices',
     'django_extensions',
+    "corsheaders",
     
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "Http://localhost:3000"
+]
